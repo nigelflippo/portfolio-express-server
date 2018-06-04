5375f2767f02914917164ba90fda20f0816a34d2
nigelflippo <flippo.nigel@gmail.com>
2018-06-04 17:54:18 -0600

    removed nodemailer

--- routes/index.js
+++ routes/index.js
@@ -8,28 +8,4 @@ const comments = require('./comments')
 router.get('/comments', comments.getAllComments)
 router.post('/comments', comments.postComment)
 
-router.post('/send', (req, res, next) => {
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth : {
-      user: 'flippo.nigel@gmail.com',
-      pass: 'isisosiris91!'
-    }
-  })
-  const mailOptions = {
-    from: 'flippo.nigel@gmail.com',
-    to: 'flippo.nigel@gmail.com',
-    subject: `${req.body.name}: ${req.body.phone}`,
-    text: `${req.body.message}`,
-    replyTo: `${req.body.email}`
-  }
-  transporter.sendMail(mailOptions, (err, res) => {
-    if (err) {
-      console.error('error', err)
-    } else {
-      console.log('email', res);
-    }
-  })
-})
-
 module.exports = router
