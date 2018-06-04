31c4e34ad203fe202cd1a2eda2c24ba36dc2dd87
nigelflippo <flippo.nigel@gmail.com>
2018-06-04 07:43:08 -0600

    initial

--- routes/index.js
+++ routes/index.js
@@ -12,12 +12,12 @@ router.post('/send', (req, res, next) => {
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth : {
-      user: 'flippo.nigel@gmail.com',
+      user: 'nigel.flippo@gmail.com',
       pass: 'Isisosiris91!'
     }
   })
   const mailOptions = {
-    from: 'flippo.nigel@gmail.com',
+    from: 'nigel.flippo@gmail.com',
     to: 'flippo.nigel@gmail.com',
     subject: `${req.body.name}: ${req.body.phone}`,
     text: `${req.body.message}`,
