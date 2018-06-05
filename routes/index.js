30ff13582f6e0977097ce3784fb9219ac6c7d8eb
nigelflippo <flippo.nigel@gmail.com>
2018-06-04 18:00:13 -0600

    nodemailer

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,28 @@ const comments = require('./comments')
 router.get('/comments', comments.getAllComments)
 router.post('/comments', comments.postComment)
 
+// router.post('/send', (req, res, next) => {
+//   const transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth : {
+//       user: '',
+//       pass: ''
+//     }
+//   })
+//   const mailOptions = {
+//     from: '',
+//     to: '',
+//     subject: `${req.body.name}: ${req.body.phone}`,
+//     text: `${req.body.message}`,
+//     replyTo: `${req.body.email}`
+//   }
+//   transporter.sendMail(mailOptions, (err, res) => {
+//     if (err) {
+//       console.error('error', err)
+//     } else {
+//       console.log('email', res);
+//     }
+//   })
+// })
+
 module.exports = router
